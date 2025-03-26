cd632b93c2ed16b0c704e685b23a8bfa6fc88b96
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-03-26 09:48:32 +0530

    added the hero, navbar and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import AboutUs from "./components/AboutUs";
 
 export default function Home() {
-    return <Header />;
+    return (
+        <div className="bg-white">
+            <AboutUs />
+        </div>
+    );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,55 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    typescript: {
+        // !! WARN !!
+        // Dangerously allow production builds to successfully complete even if
+        // your project has type errors.
+        // !! WARN !!
+        ignoreBuildErrors: true,
+    },
 
-const nextConfig: NextConfig = {
-  /* config options here */
+    // To also ignore ESLint errors during build, add:
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "cdn.dribbble.com",
+            },
+            {
+                protocol: "https",
+                hostname: "plus.unsplash.com",
+            },
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com",
+            },
+            {
+                protocol: "https",
+                hostname: "img.freepik.com",
+            },
+            {
+                protocol: "https",
+                hostname: "i.pinimg.com",
+            },
+            {
+                protocol: "https",
+                hostname: "nilufar.com",
+            },
+            {
+                protocol: "https",
+                hostname: "mir-s3-cdn-cf.behance.net",
+            },
+            {
+                protocol: "https",
+                hostname: "cdn.sanity.io",
+            },
+        ],
+    },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
