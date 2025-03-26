2fd17d46d664fb52de1956dc9d8b7e1644fdc435
Gangaprasad Pawale <10**pawalegp2003@gmail.com>
2025-03-26 15:38:11 +0530

    little changes in process component

--- app/components/ProcessCardComponent.tsx
+++ app/components/ProcessCardComponent.tsx
@@ -15,19 +15,20 @@ export const ProcessCardComponent = (
     }
 ) => {
     return (
-        <div className='flex flex-row gap-3 '>
+        <div className='flex flex-row gap-8 justify-start w-3/5'>
             <div className='flex justify-center items-center'>
                 <Image
                     src={imageUrl}
-                    width={150}
-                    height={100}
+                    width={200}
+                    height={240}
                     alt='process image'
+                    className='rounded-xl'
                 />
             </div>
-            <div className='flex flex-col gap-3' >
-                <h1>
+            <div className='flex flex-col gap-3 justify-center w-1/2' >
+                <h2 className='text-3xl font-extrabold'>
                     {title}
-                </h1>
+                </h2>
                 <p>
                     {description}
                 </p>
--- app/components/ProcessComponent.tsx
+++ app/components/ProcessComponent.tsx
@@ -3,22 +3,24 @@ import { ProcessCardComponent } from './ProcessCardComponent'
 
 export const ProcessComponent = () => {
   return (
-    <div className='flex flex-row justify-between '> 
-        <div className='w-1/2' >
+    <div className='flex flex-row justify-around p-10 gap-8 text-black font-gilroy'> 
+        <div className='w-1/3 flex flex-col gap-6' >
             <h2 className='text-4xl'>
             A complete visual merchandising tool that your teams will love.
             </h2>
             <p>
             Create the customer experience that your in-store teams can implement with precision.
             </p>
-            <button className='px-3 py-2 text-white bg-violet-500'>
+            <button className='px-1 py-2 text-white bg-violet-500'>
                 Learn More
             </button>
 
         </div>
-        <div className='flex flex-col gap-3'>
+        <div className='flex flex-col gap-13 '>
             <ProcessCardComponent title={"Review."} description={'Track every stores progress and easily give feedback and approval.'} imageUrl={'/images/2.jpg'}  className=''/>
+            <div className='border border-red-400 w-[80%]'></div>
             <ProcessCardComponent title={"Review."} description={'Track every stores progress and easily give feedback and approval.'} imageUrl={'/images/2.jpg'}  className=''/>
+            <div className='border border-red-400 w-[80%]'></div>
             <ProcessCardComponent title={"Review."} description={'Track every stores progress and easily give feedback and approval.'} imageUrl={'/images/2.jpg'}  className=''/>
 
         </div>
