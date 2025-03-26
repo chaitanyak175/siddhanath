10f387cf4b926fabc7fb237842b701e61a139bfd
Gangaprasad Pawale <10**pawalegp2003@gmail.com>
2025-03-26 09:52:01 +0530

    added footer and product component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Image from "next/image";
 import Header from "./components/Header";
+import Footer from './components/Footer'
+import ClothingDesignSteps from "./components/ClothingDesignSteps";
 
 export default function Home() {
-    return <Header />;
+    return (
+
+        <>
+            <Header />
+            <div className="h-[300vh] bg-white">
+
+            </div>
+            <ClothingDesignSteps/>
+            <Footer />
+        </>
+
+
+    );
 }
