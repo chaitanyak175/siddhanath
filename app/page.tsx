bf296400f423e85e3ccde66f6138bcf48081d31f
Gangaprasad Pawale <10**pawalegp2003@gmail.com>
2025-03-27 15:08:05 +0530

    text changes of about us

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { ProcessComponent } from "./components/ProcessComponent";
 import { useEffect } from "react";
 import Lenis from "lenis";
 import Hero from "./components/Hero";
+import AboutUs from "./components/AboutUs";
+
 
 export default function Home() {
     useEffect(() => {
@@ -38,6 +40,7 @@ export default function Home() {
                 <ClothingDesignSteps />
                 {/* <DesignProcess/> */}
                 <ProcessComponent />
+                <AboutUs />
                 <Footer />
             </div>
         </>
