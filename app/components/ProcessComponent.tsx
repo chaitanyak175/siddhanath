aba32419ca249b518891774c7acf95ec674c7d56
chaitanyak175 <karmalkarchaitanya@gmail.com>
2025-03-29 16:50:24 +0530

    made the our process text absolute

--- app/components/ProcessComponent.tsx
+++ app/components/ProcessComponent.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ProcessCardComponent } from "./ProcessCardComponent";
-import styles from './styles.module.scss'
+import styles from "./styles.module.scss";
 
 export const ProcessComponent = () => {
     return (
         <div className="relative flex flex-col sm:flex-row justify-around p-10 gap-15 sm:gap-20 text-black font-gilroy bg-[#FFFBF6] w-screen">
             {/* Sticky Left Section */}
-            <div className={`w-1/3 flex flex-col gap-6 sticky top-20 self-start h-fit ${styles.ourprocesstext}`}>
+            <div
+                className={`w-1/3 flex flex-col gap-6 sm:sticky relative top-20 self-start h-fit ${styles.ourprocesstext}`}
+            >
                 <h2 className="text-5xl sm:text-7xl font-bebas">
                     OUR PROCESS{" "}
                     <span className="hidden sm:inline-block">:</span>
